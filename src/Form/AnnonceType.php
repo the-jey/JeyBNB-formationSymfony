699c49c7a78616d5adb8d0264b548af2ea091239
the-jey <jey.pley@gmail.com>
2020-02-08 18:38:53 +0100

    Gestion des utilisateurs et liaisons avec les annonces

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Ad;
 use App\Form\ImageType;
 use App\Form\AnnonceType;
+use App\Form\ApplicationType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,26 +16,8 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
-class AnnonceType extends AbstractType
+class AnnonceType extends ApplicationType
 {
-
-    /**
-     * Permet d'avoir la configuration de base d'un champ
-     * 
-     * @param string $label
-     * @param string $placeholder
-     * @param array $options
-     * @return array
-     * 
-     */
-    private function getConfiguration($label, $placeholder, $options = []) {
-        return array_merge([
-            'label' => $label,
-            'attr' => [
-                'placeholder' => $placeholder
-            ]
-        ], $options);
-    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
