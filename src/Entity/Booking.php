75889c0e64d2ce9e0247df6ad1aadbecc2fcfae4
the-jey <jey.pley@gmail.com>
2020-02-13 21:22:05 +0100

    Mise en place de l'administration

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -33,7 +33,7 @@ class Booking
     /**
      * @ORM\Column(type="datetime")
      * @Assert\Type("datetime", message="Attention, la date d'arrivé doit être au bon format !")
-     * @Assert\GreaterThan("today", message="La date d'arrivée doit être ultérieure à la date d'aujourd'hui !")
+     * @Assert\GreaterThan("today", message="La date d'arrivée doit être ultérieure à la date d'aujourd'hui !", groups={"front"})
      */
     private $startDate;
 
@@ -63,6 +63,8 @@ class Booking
      * Callback appelé à chaque fois qu'on crée une réservation
      * 
      * @ORM\PrePersist
+     * @ORM\PreUpdate
+     * 
      * @return void
      */
     public function prePersit() {
