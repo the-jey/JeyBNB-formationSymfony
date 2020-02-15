e2b312865b659fbaef2269392a7d88f5d3f1c3a5
the-jey <jey.pley@gmail.com>
2020-02-15 17:39:11 +0100

    Mise en place de Encore pour nos css et js

--- assets/js/ad.js
+++ assets/js/ad.js
@@ -1,28 +1,28 @@
 $('#add-image').click(function(){
-    //Récupérer les futurs champs
+    // Je récupère le numéro des futurs champs que je vais créer
     const index = +$('#widgets-counter').val();
-    //Récupérer le prototype des entrées
+
+    // Je récupère le prototype des entrées
     const tmpl = $('#annonce_images').data('prototype').replace(/__name__/g, index);
-    console.log(tmpl);
+
     // J'injecte ce code au sein de la div
     $('#annonce_images').append(tmpl);
 
-    $('#widgets-counter').val(index +1);
+    $('#widgets-counter').val(index + 1);
 
-    //Je gère le bouton supprimer
+    // Je gère le bouton supprimer
     handleDeleteButtons();
 });
 
 function handleDeleteButtons() {
     $('button[data-action="delete"]').click(function(){
         const target = this.dataset.target;
-
         $(target).remove();
     });
 }
 
-function updateCounter(){
-    const count =  +$('#annonce_images .form-group').length;
+function updateCounter() {
+    const count = +$('#annonce_images div.form-group').length;
 
     $('#widgets-counter').val(count);
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,34 +11,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController {
 
     /**
-     * @Route("/salut/{prenom}/{age}", name="hello")
-     * Montre la page qui dit bonjour
-     * 
-     * @return void;
-     */
-    public function hello($prenom = 'Inconnu', $age = 0) {
-        return $this->render(
-            'hello.html.twig',
-            [
-                'prenom' => $prenom,
-                'age' => $age
-            ]
-        );
-    }
-
-    /**
      * @Route("/", name="homepage")
      */
-    public function home() {
-        $prenoms = ['Lionel' => 12, 'Jey' => 15, 'Quentih' => 25];
+    public function home(AdRepository $adRepo, UserRepository $userRepo) {        
 
         return $this->render(
             'home.html.twig', 
             [
-                'title' => "Aurevoir tout le monde !",
-                'age' => 12,
-                'tableau' => $prenoms
-                ]
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers(2)
+            ]
 
         );
     }
